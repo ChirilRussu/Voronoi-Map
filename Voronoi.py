64fb48e29127cc736a7f502646e3dd1f74e3bec5
Freezman13 <58755601+Freezman13@users.noreply.github.com>
2020-12-22 23:37:26 -0600

    First pass on a useable product
    Full map with all spawn points.
    Have 4 extra points becuase image of map gets cut off otherwise.

--- Voronoi.py
+++ Voronoi.py
@@ -3,23 +3,36 @@ import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
 import scipy.ndimage as ndimage
 
-img_file = 'respawns.png'
-img = plt.imread(img_file)
+
+img = plt.imread('respawns.jpg')
+img2 = np.fliplr(img)
 
 points = [
-[np.random.uniform(0, img.shape[0]),np.random.uniform(0, img.shape[1])], 
-[np.random.uniform(0, img.shape[0]),np.random.uniform(0, img.shape[1])], 
-[np.random.uniform(0, img.shape[0]),np.random.uniform(0, img.shape[1])], 
-[np.random.uniform(0, img.shape[0]),np.random.uniform(0, img.shape[1])]] 
+[-100, -100], [1100, -100], [-100, 1100], [1100, 1100],
+[236, 606], [282, 566], [294, 468], [330, 440],
+[360, 472], [360, 436], [406, 470], [460, 454],
+[516, 420], [526, 386], [380, 346], [340, 300],
+[454, 242], [474, 270], [522, 306], [554, 282],
+[677, 220], [790, 360], [866, 370], [740, 425],
+[805, 470], [860, 480], [825, 570], [780, 642],
+[695, 620], [560, 615], [540, 740], [780, 740],
+[725, 790], [639, 809], [650, 880], [566, 843],
+[470, 830], [453, 800], [430, 723], [445, 675],
+[357, 650], [302, 697], [257, 722], [325, 777]
+]
 
-#for i in range(100):
-#    points.append([np.random.uniform(0, img.shape[0]),np.random.uniform(0, img.shape[1])])
-#points = np.array(points)
 
 vor = Voronoi(points)
 
-fig = plt.figure(figsize=(20,20))
+fig = plt.figure(figsize=(30,30))
 ax = fig.add_subplot(111)
-ax.imshow(ndimage.rotate(img, 90))
-voronoi_plot_2d(vor, point_size=10, ax=ax)
+
+
+ax.imshow(ndimage.rotate(img2, 180))
+voronoi_plot_2d(vor,show_vertices=False, point_size=10, ax=ax)
+
 plt.show()
+
+
+
+
---
+++ Voronoi_Simple.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+img = plt.imread('respawns.png')
+plt.imshow(img)
+
+# Flip
+# img2 = np.fliplr(img)
+# plt.imshow(img2)
+
+plt.show()
